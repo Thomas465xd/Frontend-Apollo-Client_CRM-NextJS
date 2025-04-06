c67084b523cbd9ad16ba859488bd1aa2473a0455
Thomas Schrödinger <160943727+Thomas465xd@users.noreply.github.com>
2025-04-06 18:55:19 -0400

    *Main Changes -Added eslint.config rules -Created Sidebar component -Created DarkMode component to display a modern dark/light mode toggle button -Added dark mode functionality using next themes & providers -Created Loader component
    *Packages Added
    - lucide-react for icons
    - next-themes for light & dark mode
    
    *Error fixes
    -Deleted globals.css .dark class styles since they were interfering with tailwind dark: class

---
+++ components/ui/Logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+    return (
+        <div className="text-2xl font-bold text-center pt-5 pb-2 px-10 border-b border-gray-600">
+            <h1 className="text-white font-bold italic">Next CRM</h1>
+        </div>
+    )
+}
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,24 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+
+	// Adding custom rules for no-unused-vars and no-undef
+	{
+		rules: {
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": "warn",
+            
+            "no-undef": "warn",
+            "no-console": "warn",
+            "no-debugger": "warn",
+            "no-alert": "warn",
+		},
+	},
 ];
 
 export default eslintConfig;
